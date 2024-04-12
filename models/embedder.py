5f679c00cef345446b15806ab1bde108a805ec2f
ponakilan <ponakilan25@gmail.com>
2024-04-12 21:10:52 +0530

    InceptionResnet training step added

--- models/embedder.py
+++ models/embedder.py
@@ -8,15 +8,10 @@ from networks.inceptionv1 import BasicConv2d, Block35, Block17, Mixed_6a, Mixed_
 
 class InceptionResnetEmbedding(L.LightningModule):
 
-    def __init__(self, weights=None, weights_path=None, dropout_prob=0.6):
+    def __init__(self, dropout_prob, wandb_run):
         super().__init__()
 
-        self.weights = weights
-
-        if weights == 'vggface2':
-            tmp_classes = 8631
-        elif weights == 'casia-webface':
-            tmp_classes = 10575
+        self.run = wandb_run
 
         self.conv2d_1a = BasicConv2d(3, 32, kernel_size=3, stride=2)
         self.conv2d_2a = BasicConv2d(32, 32, kernel_size=3, stride=1)
@@ -59,10 +54,6 @@ class InceptionResnetEmbedding(L.LightningModule):
         self.last_linear = nn.Linear(1792, 512, bias=False)
         self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True)
 
-        if weights is not None:
-            self.logits = nn.Linear(512, tmp_classes)
-            load_weights(self, weights, weights_path)
-
     def forward(self, x):
         x = self.conv2d_1a(x)
         x = self.conv2d_2a(x)
@@ -85,26 +76,38 @@ class InceptionResnetEmbedding(L.LightningModule):
         return x
     
     def training_step(self, batch, batch_idx):
+
         anchor, positive, negative = batch
+
         a_e = self.forward(anchor)
         p_e = self.forward(positive)
         n_e = self.forward(negative)
-        loss = F.triplet_margin_loss(a_e, p_e, n_e)
-        self.log("train_loss", loss)
+
+        pos_dist = torch.norm(a_e - p_e, p=2, dim=1).detach().tolist()[0]
+        neg_dist = torch.norm(a_e - n_e, p=2, dim=1).detach().tolist()[0]
+
+        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.2)
+        self.log("train_loss", loss, sync_dist=True)
+        self.log("pos_dist", pos_dist, sync_dist=True)
+        self.log("neg_dist", neg_dist, sync_dist=True)
+        self.run.log({"train_loss": loss, "pos_dist": pos_dist, "neg_dist": neg_dist})
+
         return loss
     
+    def validation_step(self, batch, batch_idx):
+        
+        anchor, positive, negative = batch
+
+        a_e = self.forward(anchor)
+        p_e = self.forward(positive)
+        n_e = self.forward(negative)
+
+        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.2)
+        self.log("val_loss", loss, sync_dist=True)
+        self.run.log({"val_loss": loss})
+    
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.SGD(self.parameters(), lr=1e-3)
         return optimizer
 
 
-def load_weights(mdl, name, weights_path):
-    if name == 'vggface2':
-        path = f'{weights_path}/vggface2.pt'
-    elif name == 'casia-webface':
-        path = f'{weights_path}/casia-webface.pt'
-    else:
-        raise ValueError('Pretrained models only exist for "vggface2" and "casia-webface"')
-
-    state_dict = torch.load(path)
-    mdl.load_state_dict(state_dict)
