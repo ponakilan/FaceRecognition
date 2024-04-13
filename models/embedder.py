ca74bd2656c3f02716cde48d7a6a2accad718c9f
ponakilan <ponakilan25@gmail.com>
2024-04-13 12:27:56 +0530

    Lightning module removed

--- models/embedder.py
+++ models/embedder.py
@@ -81,42 +81,6 @@ class InceptionResnetEmbedding(L.LightningModule):
         x = F.normalize(x, p=2., dim=1)
         return x
     
-    def training_step(self, batch, batch_idx):
-
-        anchor, positive, negative = batch
-
-        a_e = self.forward(anchor)
-        p_e = self.forward(positive)
-        n_e = self.forward(negative)
-
-        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.8)
-
-        if batch_idx % 100 == 99:
-            pos_dist = torch.norm(a_e - p_e, p=2, dim=1).detach().tolist()[0]
-            neg_dist = torch.norm(a_e - n_e, p=2, dim=1).detach().tolist()[0]
-            self.log("train_loss", loss, sync_dist=True)
-            self.log("pos_dist", pos_dist, sync_dist=True)
-            self.log("neg_dist", neg_dist, sync_dist=True)
-            self.run.log({"train_loss": loss, "pos_dist": pos_dist, "neg_dist": neg_dist})
-
-        return loss
-    
-    def validation_step(self, batch, batch_idx):
-        
-        anchor, positive, negative = batch
-
-        a_e = self.forward(anchor)
-        p_e = self.forward(positive)
-        n_e = self.forward(negative)
-
-        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.2)
-        self.log("val_loss", loss, sync_dist=True)
-        self.run.log({"val_loss": loss})
-    
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=1e-3)
-        return optimizer
-    
 
 def load_weights(mdl):
     path = 'https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180402-114759-vggface2.pt'
