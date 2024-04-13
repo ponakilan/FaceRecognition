cdeee7b11581fa5ab69760e42e4bf2992911fadd
ponakilan <ponakilan25@gmail.com>
2024-04-13 12:06:45 +0530

    Lightning added

--- models/embedder.py
+++ models/embedder.py
@@ -3,15 +3,18 @@ import os
 import torch
 from torch import nn
 from torch.nn import functional as F
+import lightning as L
 
 from .utils.download import download_url_to_file
 from networks.inceptionv1 import BasicConv2d, Block35, Block17, Mixed_6a, Mixed_7a, Block8
 
 
-class InceptionResnetEmbedding(nn.Module):
+class InceptionResnetEmbedding(L.LightningModule):
 
-    def __init__(self, dropout_prob):
+    def __init__(self, dropout_prob, wandb_run):
         super().__init__()
+
+        self.run = wandb_run
 
         self.conv2d_1a = BasicConv2d(3, 32, kernel_size=3, stride=2)
         self.conv2d_2a = BasicConv2d(32, 32, kernel_size=3, stride=1)
@@ -54,7 +57,7 @@ class InceptionResnetEmbedding(nn.Module):
         self.last_linear = nn.Linear(1792, 512, bias=False)
         self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True)
         self.logits = nn.Linear(512, 8631)
-        
+
         load_weights(self)
 
     def forward(self, x):
@@ -77,6 +80,42 @@ class InceptionResnetEmbedding(nn.Module):
         x = self.last_bn(x)
         x = F.normalize(x, p=2., dim=1)
         return x
+    
+    def training_step(self, batch, batch_idx):
+
+        anchor, positive, negative = batch
+
+        a_e = self.forward(anchor)
+        p_e = self.forward(positive)
+        n_e = self.forward(negative)
+
+        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.8)
+
+        if batch_idx % 100 == 99:
+            pos_dist = torch.norm(a_e - p_e, p=2, dim=1).detach().tolist()[0]
+            neg_dist = torch.norm(a_e - n_e, p=2, dim=1).detach().tolist()[0]
+            self.log("train_loss", loss, sync_dist=True)
+            self.log("pos_dist", pos_dist, sync_dist=True)
+            self.log("neg_dist", neg_dist, sync_dist=True)
+            self.run.log({"train_loss": loss, "pos_dist": pos_dist, "neg_dist": neg_dist})
+
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        
+        anchor, positive, negative = batch
+
+        a_e = self.forward(anchor)
+        p_e = self.forward(positive)
+        n_e = self.forward(negative)
+
+        loss = F.triplet_margin_loss(a_e, p_e, n_e, 1.2)
+        self.log("val_loss", loss, sync_dist=True)
+        self.run.log({"val_loss": loss})
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=1e-3)
+        return optimizer
     
 
 def load_weights(mdl):
